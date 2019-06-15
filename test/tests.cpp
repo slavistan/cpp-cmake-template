652cc97650ccc516a1caaea86f68547c72ed9245
Stanislaw Hüll <Stanislaw.Huell@js-soft.com>
2019-06-16 00:39:17 +0200

    Unit test now working

--- test/tests.cpp
+++ test/tests.cpp
@@ -1,39 +1,9 @@
 #include <gsl/gsl>
-
 #include <iostream>
 
-class Foo
+#include <myproject/base.hpp>
+
+int main()
 {
- private:
-  void
-  privFunc_()
-  {
-  }
-  int priv_ar_ = 1;
-
- public:
-  Foo()
-  {
-  }
-
-  explicit Foo(int n) {}
-
-  void pubFunc() {}
-  int pub_ar2 = 2;
-};
-
-int
-main()
-{
-  Foo f1;
-  auto f2 = Foo();
-  auto f3 = Foo{};
-  Foo f4{};
-
-  Foo f5(2);
-  auto f6 = Foo(2);
-  auto f7 = Foo{2};
-  Foo f8{2};
-
-  return 1;
+  std::cout << "Hello World!" << std::endl;
 }
